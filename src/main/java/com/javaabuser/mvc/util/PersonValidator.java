608f9298ebecf83392140d8223bdc29f82289f13
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-02 15:32:35 +0300

    PersonValidator.java was created in folder util

---
+++ src/main/java/com/javaabuser/mvc/util/PersonValidator.java
@@ -0,0 +1,29 @@
+package com.javaabuser.mvc.util;
+
+import com.javaabuser.mvc.DAO.PersonDAO;
+import com.javaabuser.mvc.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class PersonValidator implements Validator {
+    private final  PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if(personDAO.getByName(person.getFullName()) != null){
+            errors.rejectValue("fullName", "Person with this full name is already exists.");
+        }
+    }
+}
