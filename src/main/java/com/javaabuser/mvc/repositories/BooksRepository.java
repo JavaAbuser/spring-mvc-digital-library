2c8e6df1cb2ae07e92ad0de33521a6ee41edb9fc
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-11 03:17:22 +0300

    added findByNameStartingWith method

---
+++ src/main/java/com/javaabuser/mvc/repositories/BooksRepository.java
@@ -0,0 +1,12 @@
+package com.javaabuser.mvc.repositories;
+
+import com.javaabuser.mvc.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByNameStartingWith(String startingWith);
+}
