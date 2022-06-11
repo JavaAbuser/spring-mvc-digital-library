b1ab1fbfd3b3a8f5729c49e3f88d02140dab0e94
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-11 03:02:44 +0300

    edited hidden filter

--- src/main/java/com/javaabuser/mvc/configuration/MyMVCDispatcherServletInitializer.java
+++ src/main/java/com/javaabuser/mvc/configuration/MyMVCDispatcherServletInitializer.java
@@ -1,10 +1,14 @@
 package com.javaabuser.mvc.configuration;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.util.EnumSet;
 
 public class MyMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -26,10 +30,21 @@ public class MyMVCDispatcherServletInitializer extends AbstractAnnotationConfigD
     public void onStartup(ServletContext servletContext) throws ServletException {
         super.onStartup(servletContext);
         registerHiddenFieldFilter(servletContext);
+        registerCharacterEncodingFilter(servletContext);
     }
 
     private void registerHiddenFieldFilter(ServletContext servletContext){
         servletContext.addFilter("HiddenHttpMethodFilter",
                 new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true,"/*");
+    }
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
     }
 }
