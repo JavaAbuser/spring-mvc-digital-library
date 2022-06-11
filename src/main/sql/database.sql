f6552130b8d387bbc35e2ea820ec45c8dfc1d6c8
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-11 03:16:52 +0300

    added takenAt field in table Book

--- src/main/sql/database.sql
+++ src/main/sql/database.sql
@@ -6,11 +6,9 @@ CREATE TABLE IF NOT EXISTS Person(
 
 CREATE TABLE IF NOT EXISTS Book(
                                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                   person_id INT,
+                                   person_id INT REFERENCES Person(id) ON DELETE SET NULL,
                                    name VARCHAR(100) UNIQUE NOT NULL,
                                    author VARCHAR(50) NOT NULL,
                                    year INT NOT NULL CHECK (year > 0),
-                                   CONSTRAINT fk_book_person_id
-                                   FOREIGN KEY (person_id) REFERENCES Person(id)
-                                   ON DELETE SET NULL
+                                   takenAt TIMESTAMP
 );
