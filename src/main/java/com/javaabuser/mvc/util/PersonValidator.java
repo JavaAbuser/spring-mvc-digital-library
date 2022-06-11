6e54c5691fa3a2aecd2cbdcfdf0b2f630b05baca
JavaAbuser <steblevskiymaxim@ua.fm>
2022-06-11 03:12:41 +0300

    edited parameters in method rejectValue

--- src/main/java/com/javaabuser/mvc/util/PersonValidator.java
+++ src/main/java/com/javaabuser/mvc/util/PersonValidator.java
@@ -1,7 +1,7 @@
 package com.javaabuser.mvc.util;
 
-import com.javaabuser.mvc.DAO.PersonDAO;
-import com.javaabuser.mvc.model.Person;
+import com.javaabuser.mvc.models.Person;
+import com.javaabuser.mvc.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -9,11 +9,11 @@ import org.springframework.validation.Validator;
 
 @Component
 public class PersonValidator implements Validator {
-    private final  PersonDAO personDAO;
+    private final PeopleService peopleService;
 
     @Autowired
-    public PersonValidator(PersonDAO personDAO) {
-        this.personDAO = personDAO;
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     @Override
@@ -24,8 +24,8 @@ public class PersonValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Person person = (Person) target;
-        if(personDAO.getByName(person.getFullName()) != null){
-            errors.rejectValue("fullName", "Person with this full name is already exists.");
+        if (peopleService.findByFullName(person.getFullName()).isPresent()) {
+            errors.rejectValue("fullName", "","Person with this full name is already exists.");
         }
     }
 }
